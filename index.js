e0b26b1d18365603ec15f69a713f9ee0926432a1
jessicamaffezzolli4-stack <jessicamaffezzolli4@gmail.com>
2026-05-06 19:43:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+   const PORT = process.env.PORT;
+
 
 // ------------------------------------------------------------
 // CONFIGURAÇÃO — lida do ambiente (.env no Railway)
